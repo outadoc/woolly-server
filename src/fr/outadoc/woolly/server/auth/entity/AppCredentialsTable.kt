43a0f4a86fcb748aca45ee52cce3ddea85fcd7b3
Baptiste Candellier <baptiste@candellier.me>
2021-05-14 22:30:14 +0200

    fix(db): fix duplicate column name

--- src/fr/outadoc/woolly/server/auth/entity/AppCredentialsTable.kt
+++ src/fr/outadoc/woolly/server/auth/entity/AppCredentialsTable.kt
@@ -5,6 +5,6 @@ import org.jetbrains.exposed.sql.Table
 object AppCredentialsTable : Table() {
     val domain = varchar("domain", 512)
     val clientId = varchar("client_id", 512)
-    val clientSecret = varchar("client_id", 512)
+    val clientSecret = varchar("client_secret", 512)
     override val primaryKey = PrimaryKey(domain)
 }
